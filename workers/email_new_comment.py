0710fe8b790198683c99770e72f49a62e0d46985
Bojan Pavlovic <pavlovicbojan86@gmail.com>
2017-03-21 10:06:36 +0100

    javaScript implementation/some html changes

--- workers/email_new_comment.py
+++ workers/email_new_comment.py
@@ -6,10 +6,10 @@ class EmailNewCommentWorker(BaseHandler):
     def post(self):
         topic_author_email = self.request.get("topic_author_email")
         topic_title = self.request.get("topic_title")
-        # comment_content = self.request.get("comment_content")
+        comment_content = self.request.get("comment_content")
 
         # send mail
         mail.send_mail(sender="pavlovicbojan86@gmail.com",
                        to=topic_author_email,
                        subject="There is new comment on your topic: %s !" % topic_title,
-                       body="Test za app error")
+                       body="Comment: %s" % comment_content)
