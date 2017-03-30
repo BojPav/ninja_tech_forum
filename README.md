53d5d3d2b3b7b5492cb068bb901ad8fb35cdbafc
Bojan Pavlovic <pavlovicbojan86@gmail.com>
2017-03-30 08:31:21 +0200

    added lightbox gallery

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Ninja Tech Forum - GAE web application.
 
+Forum web aplication about tech topics. Google login for users, cookies, CSRF protection.
+Subscribing for latest topics(CRON job). Application also sends email if your Topic is commented.
+Project deployed via Google App Engine, used Jinja templating engine and WebApp2 web aplication framework.
+Used Python for backend.
+
 http://bojpav-ninjatech-forum.appspot.com/
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from crons.delete_comments import DeleteCommentsCron
 from crons.delete_topics_cron import DeleteTopicsCron
 from crons.send_topics_subscribers import SubsTopicsCron
 from handlers.base import MainHandler, AboutHandler, CookieHandler, UserCommentsHandler
+from handlers.gallery import GalleryHandler
 from handlers.subscribers import SubscribeHandler
 from handlers.topics import TopicCreateHandler, TopicDetailsHandler, TopicDeleteHandler
 from handlers.comments import CommentAdd, CommentDelete
@@ -24,6 +25,9 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/topic/<topic_id:\d+>/comment/add', CommentAdd, name="comment-add"),
     webapp2.Route('/user-comments', UserCommentsHandler, name="user-comments"),
     webapp2.Route('/comment/<comment_id:\d+>/delete', CommentDelete, name="comment-delete"),
+
+    # gallery
+    webapp2.Route('/gallery', GalleryHandler, name="gallery"),
 
     # subscribe
     webapp2.Route('/topic-subscribe', SubscribeHandler, name="topic-subscribe"),
